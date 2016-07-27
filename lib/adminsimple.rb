0e438b2c128a0dbcebe35385bb24c45120d02cc4
Billy Griffin <navyosu@gmail.com>
2016-07-27 14:34:34 -0600

    Removed require for has_scope and inherited_resources

--- lib/adminsimple.rb
+++ lib/adminsimple.rb
@@ -1,8 +1,6 @@
 require 'simple_form'
 require 'navigasmic'
 require 'crummy'
-require 'inherited_resources'
-require 'has_scope'
 
 require 'adminsimple/version'
 require 'adminsimple/configuration'
